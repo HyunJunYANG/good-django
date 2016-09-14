7072ad20f7637fd02b98de5cc32777629ea227ac
didrns <yhj780@naver.com>
2016-09-14 19:21:59 +0900

    django

--- chat/settings.py
+++ chat/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vxd520h8d9g&)iq*gmjk3g4kp2-u@3zei@s+lso=41pd3lu(3x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -101,3 +101,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
